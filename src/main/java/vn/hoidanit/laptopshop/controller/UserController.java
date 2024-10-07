246223f17219a94e3a6839abab2bc350db10f1b4
dinh van linh <dinhvanlinh06052002@gmail.com>
2024-10-07 19:55:03 +0700

    prj

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,38 +1,40 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
-    private UserService UserService;
+    private final UserService userService;
 
-    public UserController(vn.hoidanit.laptopshop.service.UserService userService) {
-        UserService = userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("eric", "test");
+        return "hello";
+    }
 
-    public String getHomePage() {
+    @RequestMapping("/admin/user")
+
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        this.userService.handleSaveUser(hoidanit);
+        System.out.println("run here" + hoidanit);
         return "hello";
     }
 }
-// @RestController
-// public class UserController {
-// private UserService UserService;
-
-// public UserController(vn.hoidanit.laptopshop.service.UserService userService)
-// {
-// UserService = userService;
-// }
-
-// @GetMapping("/")
-
-// public String getHomePage() {
-// return this.UserService.handleHello();
-// }
-// }
